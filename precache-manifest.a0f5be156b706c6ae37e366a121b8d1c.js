9d4489a9435bd22d4c3c8d4323ad590916baec08
Kamal Hussain <kamalhussainhunzai@gmail.com>
2019-10-03 19:52:06 +0500

    Updates

---
+++ precache-manifest.a0f5be156b706c6ae37e366a121b8d1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "30a9bec9fb26743d131d",
+    "url": "/static/js/main.30a9bec9.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "30a9bec9fb26743d131d",
+    "url": "/static/css/main.010f7d12.chunk.css"
+  },
+  {
+    "revision": "5fd62f33d647e28f91dd8f3d52fcc01c",
+    "url": "/index.html"
+  }
+];
